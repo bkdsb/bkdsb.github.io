595bd8d23acb9cc3e21c202700c00eff9311e275
Bruno Kalebe <brunokalebe@gmail.com>
2022-09-21 01:08:38 -0300

    Adicionado um pouco de JS

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const spinningJs = [
+  { transform: 'rotate(0) scale(1)'},
+  {transform: 'rotate(-30deg) scale(0)'}
+]
+
+const spinningTime = {
+  duration: 2000,
+  iterations: 1,
+}
+
+
+const javaScript = document.querySelector('.javaScript');
+const javaScriptCursor = document.querySelector('.javaScript').style.cursor = "pointer";
+
+
+javaScript.addEventListener('click', () => {
+  javaScript.animate(spinningJs, spinningTime);
+  }
+  );
+ 
